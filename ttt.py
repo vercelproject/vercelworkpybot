561c985f318e9fba70ef51e428f40c934b0b3b14
maksym@gmail.com <maksymko.lette@gmail.com>
2022-08-14 11:53:39 +0300

    eleven

--- ttt.py
+++ ttt.py
@@ -67,7 +67,7 @@ async def startCount():
     messages = await client.get_messages('Data', limit=200, reverse = False)
     messages.reverse()
     for message in messages:
-        date = message.date - timedelta(hours=8)
+        date = message.date - timedelta(hours=7)
         text = message.text.replace("*", "")
 
         if date.day == datetime.now().day:    
